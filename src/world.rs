3aa4a602bea1bf96bc8206f0bcdc0ef04b47dd33
Dustin J. Mitchell <dustin@v.igoro.us>
2022-10-14 14:48:45 -0400

    ch7 - World::color_at

--- src/world.rs
+++ src/world.rs
@@ -1,4 +1,4 @@
-use crate::{Color, Intersections, Light, Mat, Material, Object, Ray, Sphere, Tup};
+use crate::{shade_hit, Color, Comps, Intersections, Light, Object, Ray, Tup};
 
 #[derive(Debug)]
 pub struct World {
@@ -29,6 +29,7 @@ impl World {
     /// Create the "default_world" from the tests.
     #[cfg(test)]
     pub(crate) fn test_world() -> Self {
+        use crate::{Mat, Material, Sphere};
         let mut w = World::default();
         w.add(Box::new(Sphere::default().with_material(Material {
             color: Color::new(0.8, 1.0, 0.6),
@@ -42,6 +43,17 @@ impl World {
                 .with_material(Material::default()),
         ));
         w
+    }
+
+    pub fn color_at(&self, ray: &Ray) -> Color {
+        let mut inters = Intersections::default();
+        self.intersect(ray, &mut inters);
+        if let Some(hit) = inters.hit() {
+            let comps = Comps::prepare(hit, ray);
+            shade_hit(&self, &comps)
+        } else {
+            Color::black()
+        }
     }
 }
 
@@ -63,5 +75,48 @@ mod test {
         assert_relative_eq!(it.next().expect("intersection").t, 5.5);
         assert_relative_eq!(it.next().expect("intersection").t, 6.0);
         assert!(it.next().is_none());
+    }
+
+    #[test]
+    fn color_at_miss() {
+        let w = World::test_world();
+        let r = Ray::new(Tup::point(0, 0, -5), Tup::vector(0, 1, 0));
+        assert_relative_eq!(w.color_at(&r), Color::black());
+    }
+
+    #[test]
+    fn color_at_hit() {
+        let w = World::test_world();
+        let r = Ray::new(Tup::point(0, 0, -5), Tup::vector(0, 0, 1));
+        assert_relative_eq!(
+            w.color_at(&r),
+            Color::new(
+                0.38066119308103435,
+                0.47582649135129296,
+                0.28549589481077575
+            )
+        );
+    }
+
+    #[test]
+    fn color_at_behind_ray() {
+        let mut w = World::default();
+        w.add(Box::new(Sphere::default().with_material(Material {
+            color: Color::new(0.8, 1.0, 0.6),
+            diffuse: 0.7,
+            specular: 0.2,
+            ambient: 1.0,
+            ..Default::default()
+        })));
+        w.add(Box::new(
+            Sphere::default()
+                .with_transform(Mat::identity().scale(0.5, 0.5, 0.5))
+                .with_material(Material {
+                    ambient: 1.0,
+                    ..Default::default()
+                }),
+        ));
+        let r = Ray::new(Tup::point(0, 0, 0.75), Tup::vector(0, 0, -1));
+        assert_relative_eq!(w.color_at(&r), Color::white());
     }
 }
