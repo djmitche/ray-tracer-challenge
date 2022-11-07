326a91a552456755ff4573e8eafa2b988332d813
Dustin J. Mitchell <dustin@v.igoro.us>
2022-11-07 12:08:14 -0500

    move light into world

--- src/world.rs
+++ src/world.rs
@@ -1,6 +1,4 @@
-use crate::{
-    spaces, Color, Intersection, Intersections, Light, Material, Object, Point, Ray, Vector,
-};
+use crate::{spaces, Color, Intersection, Intersections, Material, Object, Point, Ray, Vector};
 
 /// An index into the objects in a world.
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -9,6 +7,22 @@ pub struct ObjectIndex(usize);
 impl ObjectIndex {
     pub fn test_value(i: usize) -> ObjectIndex {
         ObjectIndex(i)
+    }
+}
+
+/// A representation of a light in the world.
+#[derive(Debug, Copy, Clone, PartialEq, Default)]
+pub struct Light {
+    pub position: Point<spaces::World>,
+    pub intensity: Color,
+}
+
+impl Light {
+    pub fn new_point(position: Point<spaces::World>, intensity: Color) -> Self {
+        Self {
+            position,
+            intensity,
+        }
     }
 }
 
@@ -99,6 +113,57 @@ impl World {
         (point, eyev, normalv, reflectv, color)
     }
 
+    /// Calculate the lighting for the given args.
+    fn lighting(
+        light: &Light,
+        color: Color,
+        material: &Material,
+        point: Point<spaces::World>,
+        eyev: Vector<spaces::World>,
+        normalv: Vector<spaces::World>,
+        in_shadow: bool,
+    ) -> Color {
+        // combine surface color and light color
+        let eff_color = color * light.intensity;
+
+        // get the direction from the point to the light source
+        let lightv = (light.position - point).normalize();
+
+        // compute the ambient contribution
+        let ambient = eff_color * material.ambient;
+
+        if in_shadow {
+            return ambient;
+        }
+
+        // calculate diffuse and specular
+        let diffuse;
+        let specular;
+
+        // light_dot_normal is the cosine of the angle between the light vector and the normal
+        // vector.  A negative number means it is on the other side of the surface.
+        let light_dot_normal = lightv.dot(normalv);
+        if light_dot_normal < 0.0 {
+            diffuse = Color::black();
+            specular = Color::black();
+        } else {
+            // compute the diffuse contribution
+            diffuse = eff_color * material.diffuse * light_dot_normal;
+
+            // reflect_dot_eye is the cosine of the angle between the reflection vector and the eye
+            // vector.
+            let reflect_dot_eye = (-lightv).reflect(normalv).dot(eyev);
+            if reflect_dot_eye < 0.0 {
+                specular = Color::black();
+            } else {
+                let factor = reflect_dot_eye.powf(material.shininess);
+                specular = light.intensity * material.specular * factor;
+            }
+        }
+
+        ambient + diffuse + specular
+    }
+
     fn point_is_shadowed(&self, point: Point<spaces::World>) -> bool {
         let to_light = self.light.position - point;
         let to_light_norm = to_light.normalize();
@@ -139,7 +204,8 @@ impl World {
         if let Some(hit) = inters.hit() {
             let obj = &self.objects[hit.object_index.0];
             let (point, eyev, normalv, reflectv, color) = Self::precompute(obj, hit, ray);
-            let surface = self.light.lighting(
+            let surface = Self::lighting(
+                &self.light,
                 color,
                 &obj.material,
                 point,
@@ -347,6 +413,84 @@ mod test {
                 0.23791324567564653,
                 0.14274794740538793
             )
+        );
+    }
+
+    #[test]
+    fn eye_between_light_and_surface() {
+        let m = Material::default();
+        let position = Point::new(0, 0, 0);
+        let eyev = Vector::new(0, 0, -1);
+        let normalv = Vector::new(0, 0, -1);
+        let light = Light::new_point(Point::new(0, 0, -10), Color::white());
+        assert_relative_eq!(
+            World::lighting(&light, Color::white(), &m, position, eyev, normalv, false),
+            Color::new(1.9, 1.9, 1.9)
+        );
+    }
+
+    #[test]
+    fn eye_45_to_normal() {
+        let m = Material::default();
+        let position = Point::new(0, 0, 0);
+        let eyev = Vector::new(0, 2f64.sqrt() / 2.0, -2f64.sqrt() / 2.0);
+        let normalv = Vector::new(0, 0, -1);
+        let light = Light::new_point(Point::new(0, 0, -10), Color::white());
+        assert_relative_eq!(
+            World::lighting(&light, Color::white(), &m, position, eyev, normalv, false),
+            Color::new(1.0, 1.0, 1.0)
+        );
+    }
+
+    #[test]
+    fn light_45_to_normal() {
+        let m = Material::default();
+        let position = Point::new(0, 0, 0);
+        let eyev = Vector::new(0, 0, -1);
+        let normalv = Vector::new(0, 0, -1);
+        let light = Light::new_point(Point::new(0, 10, -10), Color::white());
+        assert_relative_eq!(
+            World::lighting(&light, Color::white(), &m, position, eyev, normalv, false),
+            Color::new(0.7363961030678927, 0.7363961030678927, 0.7363961030678927)
+        );
+    }
+
+    #[test]
+    fn light_eye_in_path_of_reflection() {
+        let m = Material::default();
+        let position = Point::new(0, 0, 0);
+        let eyev = Vector::new(0, -2f64.sqrt() / 2.0, -2f64.sqrt() / 2.0);
+        let normalv = Vector::new(0, 0, -1);
+        let light = Light::new_point(Point::new(0, 10, -10), Color::white());
+        assert_relative_eq!(
+            World::lighting(&light, Color::white(), &m, position, eyev, normalv, false),
+            Color::new(1.6363961030678928, 1.6363961030678928, 1.6363961030678928)
+        );
+    }
+
+    #[test]
+    fn light_behind_surface() {
+        let m = Material::default();
+        let position = Point::new(0, 0, 0);
+        let eyev = Vector::new(0, 0, -1);
+        let normalv = Vector::new(0, 0, -1);
+        let light = Light::new_point(Point::new(0, 0, 10), Color::white());
+        assert_relative_eq!(
+            World::lighting(&light, Color::white(), &m, position, eyev, normalv, false),
+            Color::new(0.1, 0.1, 0.1)
+        );
+    }
+
+    #[test]
+    fn surface_in_shadow() {
+        let m = Material::default();
+        let position = Point::new(0, 0, 0);
+        let eyev = Vector::new(0, 0, -1);
+        let normalv = Vector::new(0, 0, -1);
+        let light = Light::new_point(Point::new(0, 0, -10), Color::white());
+        assert_relative_eq!(
+            World::lighting(&light, Color::white(), &m, position, eyev, normalv, true),
+            Color::new(0.1, 0.1, 0.1),
         );
     }
 }
